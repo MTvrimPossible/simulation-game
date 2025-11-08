c6160d7dd54134aafd89cd98986f3396d9f3d6e7
MTvrimPossible <bostonliterature@gmail.com>
2025-11-08 18:39:10 -0500

    Update ecs.js

--- src/ecs.js
+++ src/ecs.js
@@ -1,75 +1,21 @@
-/**
- * ecs.js v2 (With Serialization)
- */
-
 export class System {
     constructor() { this.requiredComponents = []; }
-    update(world, entities, turns_passed) { console.warn(`System ${this.constructor.name} has no update() method.`); }
+    update(world, entities, turns_passed) {}
 }
-
 export class World {
     constructor() {
-        this.nextEntityId = 1;
-        this.entities = [];
-        this.components = {};
-        this.systems = [];
-        this.isPaused = false;
-        this.playerEntityId = null; // Important to save!
+        this.nextEntityId = 1; this.entities = []; this.components = {}; this.systems = []; this.isPaused = false; this.playerEntityId = null; this.mapData = null;
     }
-
-    createEntity() {
-        const id = this.nextEntityId++;
-        this.entities.push(id);
-        return id;
-    }
-
-    addComponent(entityId, componentName, data) {
-        if (!this.components[componentName]) this.components[componentName] = {};
-        this.components[componentName][entityId] = data;
-    }
-
-    getComponent(entityId, componentName) {
-        return this.components[componentName] ? this.components[componentName][entityId] : undefined;
-    }
-
-    registerSystem(system) { this.systems.push(system); }
-
-    updateSystems(turns_passed) {
-        for (const system of this.systems) {
-            const required = system.requiredComponents;
-            const relevantEntities = this.entities.filter(entityId => {
-                return required.every(compName => {
-                    return this.components[compName] && this.components[compName][entityId] !== undefined;
-                });
-            });
-            system.update(this, relevantEntities, turns_passed);
+    createEntity() { const id = this.nextEntityId++; this.entities.push(id); return id; }
+    addComponent(id, name, data) { if (!this.components[name]) this.components[name] = {}; this.components[name][id] = data; }
+    getComponent(id, name) { return this.components[name] ? this.components[name][id] : undefined; }
+    registerSystem(sys) { this.systems.push(sys); }
+    updateSystems(turns) {
+        for (const sys of this.systems) {
+            sys.update(this, this.entities.filter(e => sys.requiredComponents.every(c => this.components[c] && this.components[c][e] !== undefined)), turns);
         }
     }
-
-// In src/ecs.js Class World:
-
-    // Update serialize:
-    serialize() {
-        return JSON.stringify({
-            nextEntityId: this.nextEntityId,
-            entities: this.entities,
-            components: this.components,
-            playerEntityId: this.playerEntityId,
-            mapData: this.mapData // <--- ADD THIS
-        });
-    }
-
-    // Update deserialize:
-    deserialize(jsonString) {
-        const data = JSON.parse(jsonString);
-        this.nextEntityId = data.nextEntityId;
-        this.entities = data.entities;
-        this.components = data.components;
-        this.playerEntityId = data.playerEntityId;
-        this.mapData = data.mapData; // <--- AND THIS
-    }
-
-    // AND ADD THIS HELPER SO GAMELOOP FINDS IT AFTER LOAD:
-    getCurrentMap() {
-        return this.mapData || [['.']];
-    }
+    serialize() { return JSON.stringify({ nextEntityId: this.nextEntityId, entities: this.entities, components: this.components, playerEntityId: this.playerEntityId, mapData: this.mapData }); }
+    deserialize(json) { const d = JSON.parse(json); this.nextEntityId = d.nextEntityId; this.entities = d.entities; this.components = d.components; this.playerEntityId = d.playerEntityId; this.mapData = d.mapData; }
+    getCurrentMap() { return this.mapData || [['.']]; }
+}
