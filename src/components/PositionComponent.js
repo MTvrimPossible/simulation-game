1270ad336d54edc95f51160811401c4d3bf60936
MTvrimPossible <bostonliterature@gmail.com>
2025-11-08 15:53:25 -0500

    Create PositionComponent for 2D grid coordinates

---
+++ src/components/PositionComponent.js
@@ -0,0 +1,11 @@
+/**
+ * PositionComponent.js
+ * Holds the 2D grid coordinates of an entity.
+ */
+
+export class PositionComponent {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+    }
+}
